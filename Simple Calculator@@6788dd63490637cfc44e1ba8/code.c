9557172d841bf55fbb54e1e240b1824c78e8c668
KhushiJindal2006 <khushi1198.becse24@chitkara.edu.in>
2025-01-30 11:37:26 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -14,7 +14,12 @@ int main() {
         printf("%d",a*b);
     }
     else if(d=='/'){
+        if(b!=0){
         printf("%d",a/b);
+        }
+        else {
+            printf("error");
+        }
     }
     else {
         printf("error");
