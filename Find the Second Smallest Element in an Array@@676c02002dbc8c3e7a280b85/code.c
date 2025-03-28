cf315212aa66bf6912f7ade35d0661c4bc84c8c5
KhushiJindal2006 <jindalkhushi9596@gmail.com>
2025-03-28 18:12:19 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -7,21 +7,40 @@ int main(){
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    int mini=arr[0];
-    int smallest=100000;
+    int smallest=arr[0];
+    int second_smallest=100000;
     for(int i=1;i<n;i++){
-        if(arr[i]<mini){
-            smallest=mini;
-        }
-        else if(arr[i]<smallest && arr[i]>mini){
+        if(arr[i]<smallest){
             smallest=arr[i];
         }
     }
-    if(smallest==100000){
+    for(int i=0;i<n;i++){
+        if(arr[i]<second_smallest && arr[i]>smallest){
+            second_smallest=arr[i];
+        }
+    }
+    if(second_smallest==100000){
         printf("-1");
     }
-    else {
-        printf("%d",smallest);
+    else if{
+        printf("%d",second_smallest);
     }
-
 }
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     int arr[n];
+//     for (int i=0; i<n; i++) {
+//         scanf("%d". &arr[i]);
+//     }
+//     for (int i=0; i<n-1; i++) {
+//         for(int j=0; j<n-i-1; j++) {
+//             if (arr[j]>arr[j+1]) {
+//                 int temp=arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]=arr[j];
+//             }
+//         }
+//     }
+
+// }
